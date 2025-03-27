ef027cd6d192cd3f37fb22b1a290762b01400035
Claire Ann Gabato <Claire Ann Gabato>
2025-03-28 01:33:08 +0800

    feat: add POST /exams endpoint

--- routes/route.js
+++ routes/route.js
@@ -29,6 +29,14 @@ router.get('/exams', (req, res) =>
   res.json(exams)
 )
 
+router.post('/exams', (req, res) => {
+  const newExam = req.body;
+  newExam.id = exams.length + 1; 
+  exams.push(newExam);
+  res.status(201).json(newExam);
+});
+module.exports = router
+
 
 
 
