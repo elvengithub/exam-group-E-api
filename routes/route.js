fc70790c099b0a482dbd2338eddec7aae9f3faa3
Claire Ann Gabato <Claire Ann Gabato>
2025-03-28 00:18:51 +0800

    added code

--- routes/route.js
+++ routes/route.js
@@ -6,34 +6,3 @@ const exams = [
   { id: 1, name: 'Math Exam', date: '2025-03-25' },
   { id: 2, name: 'Science Exam', date: '2025-03-26' }
 ];
-
-router.get('/exam-group', (req,res) => 
-  res.json({message: "Group F API"})
-)
-
-router.get('/exams', (req, res) =>
-  res.json(exams)
-)
-
-router.put('/exams/:id', (req, res) => {
-  const examId = parseInt(req.params.id);
-  const updatedData = req.body;
-
-  let exam = exams.find(e => e.id === examId);
-  if (exam) {
-    exam.name = updatedData.name || exam.name;
-    exam.date = updatedData.date || exam.date;
-    res.json(exam);
-  } else {
-    res.status(404).json({ error: 'Exam not found' });
-  }
-});
-
-
-router.post('/exams', (req, res) => {
-  const newExam = req.body;
-  newExam.id = exams.length + 1; 
-  exams.push(newExam);
-  res.status(201).json(newExam);
-});
-module.exports = router;
