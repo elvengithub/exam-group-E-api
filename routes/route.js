797437fdb33f77a07b42cd0c7baba6dda408394f
Claire Ann Gabato <Claire Ann Gabato>
2025-03-28 00:43:53 +0800

    feat: add GET /exams endpoint

--- routes/route.js
+++ routes/route.js
@@ -6,3 +6,11 @@ const exams = [
   { id: 1, name: 'Math Exam', date: '2025-03-25' },
   { id: 2, name: 'Science Exam', date: '2025-03-26' }
 ];
+
+router.get('/exam-group', (req,res) => 
+  res.json({message: "Group E API"})
+)
+
+router.get('/exams', (req, res) =>
+  res.json(exams)
+)
